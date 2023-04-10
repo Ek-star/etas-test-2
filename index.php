f240368b5100838cfd912bfc1ddd36d3ecff6f6e
Ek-star <totoekalma@gmail.com>
2023-04-11 00:58:15 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
   <h1>Hello World</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis non quia animi dolores tenetur harum? Consequatur accusamus numquam ad aliquam ut, molestias vitae quam. Deleniti nemo magnam fugiat consequatur dolore!</p>
 </body>
 </html>
